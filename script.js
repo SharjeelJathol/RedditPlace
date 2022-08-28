fa8dd081d457f6ed7039581eb613993c8c903385
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2022-08-28 13:50:05 +0500

    Options to select colour
    Added div in html file containing the input command for colour selection from user.  Using the objects instead of index for ease in renewing the events. After every new tile selected previous one will be reset to it's default position by removing border, hiding options for colour selection and calling again the initial function to add the whole event listener again named addBorder() which takes object as an argument.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let tiles;
 let tilesObject = document.querySelector('.tiles');
 let selectedTile;
+let colorSelector = document.querySelector('.optionsRemove');
 const windowSize = 10;
 
-let updateSelectedTile = i => {
-    if (selectedTile != null)
+
+let updateSelectedTile = tile => {
+    if (selectedTile != null){
         selectedTile.classList.remove('border');
-    selectedTile = tiles[i];
+        colorSelector.classList.replace('options', 'optionsRemove');
+        addBorder(selectedTile)
+    }
+    
+    selectedTile = tile;
     selectedTile.classList.add('border');
+    selectedTile.addEventListener('click', () => {
+        colorSelector.classList.replace('optionsRemove', 'options');
+    })
 }
 
 let createTiles = () => {
@@ -18,14 +27,68 @@ let createTiles = () => {
     tiles = document.querySelectorAll('.tile');
     //add event listener to those tiles
     for (let i = 0; i < tiles.length; i++)
-        addBorder(i)
+        addBorder(tiles[i])
 }
 
-let addBorder = i => {
-    tiles[i].addEventListener('click', () => {
-        updateSelectedTile(i);
+let addBorder = tile => {
+    tile.addEventListener('click', () => {
+        updateSelectedTile(tile);
     })
 }
 
 createTiles();
 
+
+
+
+
+
+
+
+
+// let buttons = document.getElementsByClassName('tile')
+// let option = document.querySelector(".optionsRemove")
+// let select = document.querySelector('.submit')
+// select.addEventListener('click', () => {
+//     console.log(document.querySelector('#color'))
+//     console.log(document.querySelector('#color').getAttribute('value'))
+
+//     // console.log(select)
+//     // console.log(select.getAttribute('value'))
+// })
+// // document.getElementsByClassName('options')
+// const buttonsLength = buttons.length
+// console.log(option.className)
+
+
+// select.addEventListener('click', () => {
+//     option.classList.replace('options', 'optionsRemove')
+// })
+
+// for (let index = 0; index < buttonsLength; index++) {
+//     buttons[index].addEventListener('click', (element) => {
+//         console.log(option.className)
+//         if (option.className === 'options')
+//             option.classList.replace('options', 'optionsRemove')
+//         else
+//             option.classList.replace('optionsRemove', 'options')
+//         // console.log(buttons[index].classList)
+//         // console.log(element)
+//         // console.log(element.srcElement)
+//     })
+//     // console.log(buttons[index])
+// }
+
+
+
+
+
+
+// for (const iterator in buttons) {
+//     console.log(element)
+// }
+
+// buttons.forEach(element => {
+//     console.log(element)
+
+// });
